d505870f57142951f834884340f4415c3768f7ed
dondonai <adones.kjc@protonmail.com>
2025-12-09 13:09:36 +0800

    feat: update image configuration and add pnpm workspace file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["storage.enfenetydev.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.enfenetydev.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
     imageSizes: [384, 512, 640, 750, 1080, 1200, 1920],
   },
 };
